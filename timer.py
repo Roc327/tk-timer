d9fff32fe929d76ccae71d7b864608dd0b4ba0ec
Roc327 <nate_van@hotmail.com>
2023-10-20 16:04:05 -0400

    Refactored more of the countdown and made cancel reflect to user canceled or completed, cleaned up unused code

--- timer.py
+++ timer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Simple Timer project to continue learning after fundamentals, using this to also learn and play with tkinter.
 
@@ -24,35 +25,8 @@ Simple Timer project to continue learning after fundamentals, using this to also
 import time
 import tkinter as tk
 from tkinter import *
-from time import strftime
-from datetime import datetime, timedelta
 
-running = True  #Global flag to exit loop
-
-
-def current_time():
-    """
-    Print current time on window
-    """
-    string = strftime('%H:%M:%S %p')
-    printed_time_lbl.config(text=string)
-    printed_time_lbl.after(1000, current_time)
-    # now = datetime.now()
-    # current_time = now.utctime("%H:%M:%S")
-    # tk.Label(text=current_time).pack()
-
-
-def check_type(var):
-    """
-    Used for debugging
-    """
-    if var == 'None':
-        print(var, '\n', type(var), ' in if')
-        return '00'
-    elif var is None:
-        print('In Elif var is ', type(var))
-    else:
-        return var
+RUNNING = True  # Global to exit loop
 
 
 def get_input():
@@ -60,69 +34,93 @@ def get_input():
     Get the inputs of the entry boxes once
     the set countdown button is pressed
     """
-    hour = entry_hour.get()
-    minute = entry_minutes.get()
-    second = entry_seconds.get()
-    time_string = (hour + ':' + minute + ':' + second)
-    ts = datetime.strptime(time_string, '%H:%M:%S')
-    time_seconds = ts.second + ts.minute * 60 + ts.hour * 3600
-    countdown(time_seconds)
-    # tk.Label(text="Time-Up", font=('bold', 20)).place(x=250, y=290)
+    if entry_hours.get() == "":
+        hours = 0
+    else:
+        hours = int(entry_hours.get())
+    if entry_minutes.get() == "":
+        minutes = 0
+    else:
+        minutes = int(entry_minutes.get())
+    if entry_seconds.get() == "":
+        seconds = 0
+    else:
+        seconds = int(entry_seconds.get())
+    countdown(hours, minutes, seconds)
 
 
-def countdown(time_seconds):
+def countdown(hours, minutes, seconds):
     """
     The main countdown function. Takes in time in seconds,
-    converts the time to a readable format HH:MM:SS,
-    then prepares it to be updated to the label then increment the loop
+    then increment the loop
     """
-    global running
-    running = True    
+    global RUNNING
+    RUNNING = True
+
+    time_seconds = seconds + (minutes * 60) + (hours * 3600)
+
     while time_seconds:
+        display_timer(time_seconds)
         window.update()
-        if running == True:
-            timer = timedelta(seconds=time_seconds)
-            timer_label.config(text=timer)
+        time_seconds -= 1
+        if RUNNING:
             time.sleep(1)
-            time_seconds -= 1
             continue
         else:
             break
 
-    timer_label.config(text="Timer Complete!")
+    if RUNNING:
+        timer_label.config(text="Timer Complete!")
+    else:
+        timer_label.config(text="Timer has been canceled.")
+
+
+def display_timer(time_seconds):
+    """
+    Convert time in seconds that is passed into function
+    into hours, minutes, seconds then display on screen
+    """
+    minutes, seconds = divmod(time_seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    time_str = f"{hours:d}:{minutes:02d}:{seconds:02d}"
+
+    timer_label.config(text=time_str)
 
 
 def exit_loop():
     """
     Exits loop if user presses cancel button
     """
-    global running
-    running = False
-    return running 
+    global RUNNING
+    RUNNING = False
+    return RUNNING
 
 
 # Creating a window, setting its size and title,
 window = tk.Tk()
-window.geometry('600x600')
-window.title('Timer')
+window.geometry("600x600")
+window.title("Timer")
 
 # create widgets
-head = tk.Label(window, text="Countdown clock and Timer", font='Calibri 15')
-enter_time_lbl1 = tk.Label(window, text="Enter time in HH:MM:SS", font='bold')
-enter_time_lbl2 = tk.Label(window, text="Must be less than 24 hrs.")
-entry_hour = tk.Entry(window, width=30)
+head = tk.Label(window, text="Countdown clock and Timer", font="Calibri 15")
+enter_time_lbl1 = tk.Label(window, text="Enter time in HH:MM:SS", font="bold")
+entry_hours = tk.Entry(window, width=30)
 entry_minutes = tk.Entry(window, width=30)
 entry_seconds = tk.Entry(window, width=30)
 checkbox_var = tk.StringVar()
-checkbox = tk.Checkbutton(window, text='Check for timer complete sound.',
-                          onvalue=True, offvalue=False,
-                          variable=checkbox_var)  # creating checkbox
-button = tk.Button(window, text="Set Countdown", command=get_input,
-                   bg='yellow')
+checkbox = tk.Checkbutton(
+    window,
+    text="Check for timer complete sound.",
+    onvalue=True,
+    offvalue=False,
+    variable=checkbox_var,
+)  # creating checkbox
+button = tk.Button(window, text="Set Countdown", command=get_input, bg="yellow")
 current_time_txt = tk.Label(window, text="Current Time:")
-printed_time_lbl = Label(font=('calibri', 15, 'bold'))
+printed_time_lbl = Label(font=("calibri", 15, "bold"))
 timer_txt_label = tk.Label(window, text="Coundown timer:")
-timer_label = tk.Label(window, font=('calibri', 15, 'bold'))
+timer_label = tk.Label(window, font=("calibri", 15, "bold"))
 cancel_button = tk.Button(window, text="Cancel", command=exit_loop)
 
 
@@ -130,22 +128,18 @@ cancel_button = tk.Button(window, text="Cancel", command=exit_loop)
 head.pack(pady=20)
 
 enter_time_lbl1.pack()
-enter_time_lbl2.pack()
 
-entry_hour.pack()
+entry_hours.pack()
 entry_minutes.pack()
 entry_seconds.pack()
 
 checkbox.pack()
 button.pack()  # button.place(x=260, y=230)
-current_time_txt.pack()
 printed_time_lbl.pack()
 
 timer_txt_label.pack()
 timer_label.pack()
 cancel_button.pack()
 
-
-current_time()
 
 window.mainloop()
